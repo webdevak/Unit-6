0d34321894098c537c6c91699efbf37b6fca0b2d
Hakanni <66316345+webdev32@users.noreply.github.com>
2020-08-08 12:30:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,31 @@ const phraseDiv = document.querySelector('#phrase');
 const qwertyDiv = document.querySelector('#qwerty');
 const missed = 0;
 const overLayDiv = document.querySelector('#overlay');
+const ul = document.querySelector('ul');
 
 
 
 const startButton = document.querySelector(".btn__reset");
+// // Listen for the start game button to be pressed
+startButton.addEventListener('click', (e) => {
+    const button = e.target;
+    if (button) {
+    overLayDiv.style.display = 'none';
+} else if (missed = 0) {
+    overLayDiv.style.display = '';
+} else {
+    overLayDiv.style.display = '';
+}
+
+});
 
 
 const phrases = [
     'Its not what you know but who you know',
-    'Nice guys finish last',
+    'Houston We have a problem',
     'If at first you dont succeed try try again',
-    'They say good things come to those who wait so imma be about an hour late',
-    'I shoot at you actors like movie directors'
+    'A Dime a Dozen',
+    'Back to square one'
 ]
 
 
@@ -21,20 +34,25 @@ const getRandomPhraseAsArray = arr => {
     // This function randomly chooses a phrase from the phrases array.
     let randomNumber =  Math.floor(Math.random() * arr.length);
     let randomPhrase = phrases[randomNumber];
-    
-    for (let i = 0; i < randomPhrase.length; i++) {
-       return randomPhrase ; 
-    }
- 
+    return randomPhrase;
 }
 
-console.log(getRandomPhraseAsArray(phrases));
+const phraseArray = getRandomPhraseAsArray(phrases);
 
+ // adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+    let character = '';
+     for (let i = 0; i < arr.length; i++) {
+        character += `<li>${arr[i]}</li>`;
+    }
+    let li = character;
+    return li;
+}
 
-// // adds the letters of a string to the display
-// const addPhraseToDisplay = arr => {
+// // Listen for the onscreen keyboard to be clicked
+// qwertyDiv.addEventListener('click', (e) => {
 
-// }
+// });
 
 
 
@@ -53,24 +71,3 @@ console.log(getRandomPhraseAsArray(phrases));
 // const checkWin = () => {
 
 // }
-
-
-// // Listen for the start game button to be pressed
-// startButton.addEventListener('click', (e) => {
-//     const button = e.target;
-//     if (button) {
-//     overLayDiv.style.display = 'none';
-// } else if (missed = 0) {
-//     overLayDiv.style.display = '';
-// } else {
-//     overLayDiv.style.display = '';
-// }
-
-// });
-
-
-
-// // Listen for the onscreen keyboard to be clicked
-// qwertyDiv.addEventListener('click', (e) => {
-
-// });
